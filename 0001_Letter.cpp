06843a0f39e156ee65c4bfb1fc2a6479a458a77f
Fernando Marques <fsouzamarques@outlook.com>
2026-02-28 20:23:29 +1100

    Create 0001_Letter.cpp with a personal letter

---
+++ 0001_Letter.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() 
+{
+  
+  std::cout << "Dear Self,\n";
+  std::cout << "My goal is to become a developer that actually know what he's doing and feel comfortable facing new challenges. That gets enough knowledge to be confident and find a job in the future.\n";
+  std::cout << "Fernando de Souza Marques - 28/02/26";
+  
+  
+}
