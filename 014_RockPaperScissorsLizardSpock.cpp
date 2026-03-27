5677e854662a37ee1d98383327e465d34af91f1b
Fernando Marques <fsouzamarques@outlook.com>
2026-03-27 22:34:10 +1100

    Implement game logic for extended RPS variant
    Add game logic for Rock, Paper, Scissors, Lizard, Spock.

---
+++ 014_RockPaperScissorsLizardSpock.cpp
@@ -0,0 +1,15 @@
+/* The logic:
+
+Scissors cuts Paper.
+Paper covers Rock.
+Rock crushes Lizard.
+Lizard poisons Spock.
+Spock smashes Scissors.
+Scissors decapitate Lizard.
+Lizard eats Paper.
+Paper disproves Spock.
+Spock vaporizes Rock.
+(and as it always has) Rock crushes Scissors.
+We will first tackle Rock, Paper, Scissors first, before making it into Rock, Paper, Scissors, Lizard, Spock! *\
+
+  
