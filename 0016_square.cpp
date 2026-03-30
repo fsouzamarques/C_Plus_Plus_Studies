7f3f5e31c2bce1044e1a2671940706ab0834f1cd
Fernando Marques <fsouzamarques@outlook.com>
2026-03-30 23:14:19 +1100

    Add loop to calculate squares from 1 to 20
    Implemented a while loop to calculate and display the square of numbers from 1 to 20.

--- 0016_square.cpp
+++ 0016_square.cpp
@@ -1,2 +1,24 @@
 /* Create a program that calculates the square of the number on a sequence from 1 to 20 using a loop */
 
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  int i = 0;
+  int square = 0;
+  
+  // Write a while loop here:
+  
+  while (i < 21) {
+
+    square = i * i;
+
+    cout << i << " " << square << endl;
+
+    i++;
+
+      }
+  
+  
+}
