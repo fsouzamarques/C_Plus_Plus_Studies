365a8a96a14d077a7e6c23d0353fbb3b173409de
Fernando Marques <fsouzamarques@outlook.com>
2026-03-30 23:03:08 +1100

    Add number guessing game with while loop
    Implemented a while loop for guessing a number with a maximum of 50 tries.

--- 015_Loops.cpp
+++ 015_Loops.cpp
@@ -1,1 +1,34 @@
 //Loops.cpp file creation
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int guess;
+  
+  int tries = 0;
+
+  cout << "I have a number 1-10.\n";
+  cout << "Please guess it: ";
+  cin >> guess;
+ 
+  // Write a while loop here:
+  
+  while (guess !=8 && tries < 50) {
+
+  cout << "Wrong guess, try again: " << endl;
+  cin >> guess;
+
+  tries++;
+
+  }
+  
+  
+  if (guess == 8) {
+    
+    std::cout << "You got it!\n";
+  
+  }  
+  
+}
