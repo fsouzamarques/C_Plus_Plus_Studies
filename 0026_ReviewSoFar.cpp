063dbf90e1088fda199f152c167c697a87d1f3fb
Fernando Marques <fsouzamarques@outlook.com>
2026-04-06 17:39:47 +1000

    Create 0026_ReviewSoFar.cpp

---
+++ 0026_ReviewSoFar.cpp
@@ -0,0 +1,20 @@
+/*Review of C++ Functions
+
+Wow! Check out all you’ve learned about C++ functions:
+
+A function is a named group of statements that do something together.
+Functions
+Preview: Docs Loading link description
+ allow you to create more flexible, modular, and DRY code.
+C++ has many built-in functions that you can use.
+Functions are called like function_name();
+A function has a declaration with a return type and possible parameters.
+A function has a definition (or body) with a group of statements and a possible return value.
+void functions do not have return values.
+Functions with a return value have return statements.
+Parameters are 
+variables
+Preview: Docs Loading link description
+ used as placeholders for function input values.
+Arguments are a function’s actual input values.
+You now know enough C++ to create some pretty cool projects on your own. But, as you’ll see, there are still many ways to improve your code.*/
