25c5f6462966bffc822e18a7492f799d3b4de067
Fernando Marques <fsouzamarques@outlook.com>
2026-04-06 16:15:10 +1000

    First version, without the doubles yet

--- 0022_WhaleTalk.cpp
+++ 0022_WhaleTalk.cpp
@@ -8,3 +8,41 @@ The u‘s and e‘s are extra long, so we must double them.
 Once we have converted text to the whale language, the result is sung slowly, as is custom in the ocean.
 
 Write a whale.cpp program that accomplishes this translation using loops and vectors.*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+string phrase = "turpentine and turtles";
+
+vector<char> vowels = {'a', 'e', 'i', 'o', 'u'};
+
+vector<char> result;
+
+for (int i = 0; i < phrase.size(); i++) {
+
+for (int j = 0; j < vowels.size(); j++) {
+
+if (phrase[i] == vowels[j]) {
+
+  result.push_back(phrase[i]);
+}
+
+}
+
+}
+
+for (int m = 0; m <result.size(); m++) {
+
+cout << result[m] << endl;
+
+}
+
+  return 0;
+}
+
+//first version done
