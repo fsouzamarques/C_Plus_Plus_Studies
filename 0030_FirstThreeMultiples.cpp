83765a282a5d2d1de47d17735c17ebc47a379393
Fernando Marques <fsouzamarques@outlook.com>
2026-04-07 20:06:35 +1000

    Create 0030_FirstThreeMultiples.cpp

---
+++ 0030_FirstThreeMultiples.cpp
@@ -0,0 +1,27 @@
+/*Write a function named first_three_multiples() that has:
+
+An int parameter named num.
+The function should return an std::vector of the first three multiples of num in ascending order.
+
+For example, first_three_multiples(7) should return a vector with 7, 14, and 21.*/
+
+#include <iostream>
+#include <vector>
+
+// Define first_three_multiples() here:
+
+std::vector<int> first_three_multiples(int num) {
+  
+  std::vector<int> multiples{num, num * 2, num * 3};
+  
+  return multiples;
+
+}
+
+int main() {
+  
+  for (int element : first_three_multiples(8)) {
+    std::cout << element << "\n";
+  }
+  
+}
