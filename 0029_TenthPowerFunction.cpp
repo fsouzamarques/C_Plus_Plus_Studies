2390d5d44c3fe82a49244d51279f6cee613d6a2a
Fernando Marques <fsouzamarques@outlook.com>
2026-04-07 20:01:35 +1000

    Create 0029_TenthPowerFunction.cpp

---
+++ 0029_TenthPowerFunction.cpp
@@ -0,0 +1,25 @@
+/*Write a function named tenth_power() that has:
+
+An int parameter named num.
+The function should return num raised to the 10th power.*/
+
+#include <iostream>
+#include <cmath>
+
+// Define tenth_power() here:
+
+int tenth_power(int num) {
+
+num = num * num * num * num * num * num * num * num * num * num;
+
+return num;
+
+}
+
+int main() {
+  
+  std::cout << tenth_power(0) << "\n";
+  std::cout << tenth_power(1) << "\n";
+  std::cout << tenth_power(2) << "\n";
+  
+}
