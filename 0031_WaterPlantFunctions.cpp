c1e8c5523926be05680d4a9684c6483bf3382794
Fernando Marques <fsouzamarques@outlook.com>
2026-04-07 20:10:54 +1000

    Create 0031_WaterPlantFunctions.cpp

---
+++ 0031_WaterPlantFunctions.cpp
@@ -0,0 +1,12 @@
+/*Define a function needs_water() that accepts:
+
+First argument: An int number of days since the previous watering.
+Second argument: A bool value is_succulent. (A value of true would indicate that the plant is a succulent.)
+Inside the function, you’ll need some conditional logic:
+
+If is_succulent is false and days is greater than 3, return "Time to water the plant.".
+If is_succulent is true and days is 12 or less, return "Don't water the plant!".
+If is_succulent is true and days is greater than or equal to 13, return "Go ahead and give the plant a little water.".
+Otherwise, return "Don't water the plant!".
+Note: Don’t print the strings; return them from the function.*/
+
